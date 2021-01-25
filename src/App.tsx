ad09ac91d4e00cc1a518325a57f6597106fe5b20
NewarkX <brunowmanha@gmail.com>
2021-01-25 12:09:01 -0300

    Add NavBar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import NavBar from './core/components/NavBar';
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
