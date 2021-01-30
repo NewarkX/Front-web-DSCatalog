af26e63088ac0c19b8806732b05f5a170c774bea
NewarkX <brunowmanha@gmail.com>
2021-01-30 10:14:46 -0300

    catalog loaders ok

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 
@@ -11,16 +12,22 @@ const Catalog = () => {
     //quando a lista de produtos estiver disponivel,popular um estado no componente e  listar os produtos dinamicamente
     const [productsResponse,setProductsResponse] = useState<ProductsResponse>();
 
-    
-    
+    const [isLoading,setIsLoading] = useState(false);
+
     //quando o componente iniciar,busca a lista de produtos
     useEffect(() =>{ 
         const params = {
             page:0,
             linesPerPage: 12
         }
+        //iniciar o loader
+        setIsLoading(true);
         makeRequest({url:'/products',params })
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() => {
+            //finalizar o loader
+            setIsLoading(false);
+        })
     },[]);
 
 
@@ -30,9 +37,13 @@ const Catalog = () => {
             Catálogo de produtos 
             </h1>
         <div className="catalog-products" >
-           {productsResponse?.content.map(product => (
-               <Link to={`/products/${product.id}`} key={product.id} ><ProductCard product={product} /></Link> 
-           ))}
+            {isLoading ?  <ProductCardLoader /> : (
+                productsResponse?.content.map(product => (
+                    <Link to={`/products/${product.id}`} key={product.id} >
+                        <ProductCard product={product} />
+                    </Link> 
+                ))
+           )}
         </div>  
     </div>
     );
